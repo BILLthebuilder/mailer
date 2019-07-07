7e3b2887075b7c7bd27cd84fbb4700a147677517
BILLthebuilder <billkariri@gmail.com>
2019-07-07 14:01:21 +0300

    Updates

---
+++ precache-manifest.9a74a718f531941a766c6c22d3176837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab28b78475e08404e152a1f2c5b7252",
+    "url": "/BILLthebuilder/mailer/index.html"
+  },
+  {
+    "revision": "079d6f1b646abed564b1",
+    "url": "/BILLthebuilder/mailer/static/css/main.78a9e465.chunk.css"
+  },
+  {
+    "revision": "652e691c10dd16569187",
+    "url": "/BILLthebuilder/mailer/static/js/2.8b429a8f.chunk.js"
+  },
+  {
+    "revision": "079d6f1b646abed564b1",
+    "url": "/BILLthebuilder/mailer/static/js/main.e863e23c.chunk.js"
+  },
+  {
+    "revision": "e731e35bd12629541f10",
+    "url": "/BILLthebuilder/mailer/static/js/runtime~main.9590a3e1.js"
+  }
+]);
